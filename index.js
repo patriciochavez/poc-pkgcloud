3d1e3b78e225a6c2aa57cc624a51340d1a8ed825
Your Name <you@example.com>
2016-11-17 12:36:36 -0300

    html micro portal

--- index.js
+++ index.js
@@ -33,7 +33,43 @@ app.use(bodyParser.json());
 
 app.get(/^(.+)$/, function(req, res){ 
 	switch(req.params[0]) {
-		case '/servers':
+		case '/misservers':
+			break;
+	default: 
+			res.sendFile( __dirname + req.params[0]); 
+	}
+ });
+
+app.post(/^(.+)$/, function(req, res){ 
+	switch(req.params[0]) {
+		case '/tokens':
+			var requestData = {"auth":{"tenantName": "demo","passwordCredentials":{"username": req.body.username,"password": req.body.password}}};
+			var headers = {
+                                'Content-Type':'application/json'
+                        	}
+			var options = {
+ 				url: 'http://10.105.231.101:5000/v2.0/tokens',
+				method: 'POST',
+				headers: headers,
+				json: requestData
+				}
+			request(options, function (error, response, body) {
+			console.log("POST: /tokens");
+			    if (!error && response.statusCode == 200) {
+				var autenticado = new Object();
+				autenticado.tokenid = body.access.token.id;
+				autenticado.tenantid = body.access.token.tenant.id;
+				console.log(JSON.stringify(autenticado));
+				res.status(200).send(JSON.stringify(autenticado));
+				res.end();
+			    } else {
+				console.log("error: " + error);
+				res.status(404);
+				res.end();
+				} 
+			});
+			break;
+		case '/misservers':
 			var headers = {
 				'Content-Type':'application/json',
 				'X-Auth-Token': req.body.tokenid
@@ -52,33 +88,6 @@ app.get(/^(.+)$/, function(req, res){
                                 res.end();
                         });
                         break;
-	default: 
-	}
- });
-
-app.post(/^(.+)$/, function(req, res){ 
-	switch(req.params[0]) {
-		case '/tokens':
-			var requestData = {"auth":{"passwordCredentials":{"username": req.body.username,"password": req.body.password}}};
-			var headers = {
-                                'Content-Type':'application/json'
-                        	}
-			var options = {
- 				url: 'http://10.105.231.101:5000/v2.0/tokens',
-				method: 'POST',
-				headers: headers,
-				json: requestData
-				}
-			request(options, function (error, response, body) {
-			    if (!error && response.statusCode == 200) {
-				var tokenid = (body.access.token.id);
-				res.status(200).send(tokenid);
-			    } else {
-				res.status(404);
-				} 
-				res.end();
-			});
-			break;
 		case '/instancias':
 			
 			break;
